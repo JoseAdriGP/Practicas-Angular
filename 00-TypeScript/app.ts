7ecd185a64b2d3da285087ca3e4cee2e859529d1
José Adrián Garrido Pueras <jose.a.garrido.p@gmail.com>
2020-12-02 18:59:46 +0100

    Empezando con la primera aplicación de angular

--- 00-TypeScript/app.ts
+++ 00-TypeScript/app.ts
@@ -1,32 +1,34 @@
 (()=>{
 
   // Uso de Let y Const
-  var nombre = 'Ricardo Tapia';
-  var edad = 23;
+  const nombre = 'Ricardo Tapia';
+  let edad = 23;
 
-  var PERSONAJE = {
-    nombre: nombre,
-    edad: edad
+  const PERSONAJE = {
+    nombre,
+    edad
   };
 
 
   // Cree una interfaz que sirva para validar el siguiente objeto
-  var batman = {
+  interface Batman{
+    nombre: string;
+    artesMarciales: string[];
+  }
+  const batman = {
     nombre: 'Bruno Díaz',
     artesMarciales: ['Karate','Aikido','Wing Chun','Jiu-Jitsu']
   }
 
   // Convertir esta funcion a una funcion de flecha
-  function resultadoDoble( a, b ){
-    return (a + b) * 2
-  }
+  const resultadoDoble = ( a: number, b: number ) => (a + b) * 2
 
   // Función con parametros obligatorios, opcionales y por defecto
   // donde NOMBRE = obligatorio
   //       PODER  = opcional
   //       ARMA   = por defecto = 'arco'
-  function getAvenger( nombre, poder, arma ){
-    var mensaje;
+  function getAvenger( nombre: string, poder?: string, arma: string = "arco" ){
+    let mensaje;
     if( poder ){
       mensaje = nombre + ' tiene el poder de: ' + poder + ' y un arma: ' + arma;
     }else{
@@ -42,6 +44,11 @@
   // También un método que calcule el área  =  base * altura,
   // ese método debe de retornar un numero.
 
+  class Rectangulo{
+    constructor( public base:number, public altura: number){}
+
+    calcularArea= () => this.base * this.altura;
+  }
 
 
 })();
