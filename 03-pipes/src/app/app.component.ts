52e2b1b39fd3e00674213b9b41c0415f1bc59ae7
José Adrián Garrido Pueras <jose.a.garrido.p@gmail.com>
2020-12-03 19:57:48 +0100

    Pipe: Async

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -8,14 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'pipes';
   nombre: string = 'Capitán América';
-
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
   PI: number = Math.PI;
-
   porcentage: number = 0.234;
-
   salario: number = 1234.5;
+
+  valorPromesa = new Promise<string>( (resolve) => {
+    setTimeout( () => {
+      resolve('Llegó la data');
+    }, 4000);
+  });
 
   heroe = {
     nombre: 'Logan',
