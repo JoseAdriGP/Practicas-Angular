6b3b3895d585825b2038c6736835df925889e9f8
José Adrián Garrido Pueras <jose.a.garrido.p@gmail.com>
2020-12-04 10:08:14 +0100

    Pipe: Date (configuracion para poner en español)

--- 03-pipes/src/app/app.module.ts
+++ 03-pipes/src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import { registerLocaleData } from '@angular/common';
+import  localeEs  from '@angular/common/locales/es';
+
+
+registerLocaleData(localeEs);
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
