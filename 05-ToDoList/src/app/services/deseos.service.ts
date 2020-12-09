3c3ca485a4457f0ebf89849881ea4c6c5abfec1d
José Adrián Garrido Pueras <jose.a.garrido.p@gmail.com>
2020-12-09 13:01:24 +0100

    Hacer persistente la información con local storage

--- 05-ToDoList/src/app/services/deseos.service.ts
+++ 05-ToDoList/src/app/services/deseos.service.ts
@@ -9,10 +9,25 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() { 
-    const lista1 = new Lista('Piedras a conseguir');
-    const lista2 = new Lista('Heroes a vencer');
-
-    this.listas.push(lista1, lista2);
-
+    this.cargarStorage();
   }
+
+  crearLista(titulo: string) {
+    const nuevaLista = new Lista(titulo);
+
+    this.listas.push(nuevaLista);
+    this.guardarStorage();
+  }
+
+  guardarStorage(){
+    localStorage.setItem( 'data', JSON.stringify(this.listas) );
+  }
+  
+  cargarStorage(){
+    if( localStorage.getItem('data') ) {
+      this.listas = JSON.parse( localStorage.getItem('data') );
+    }
+    
+  }
+
 }
